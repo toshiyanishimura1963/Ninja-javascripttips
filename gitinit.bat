dde51b297cc1059625086200e548304bbf8e0eb2
toshiyanishimura1963 <toshiya.nishimura@gmail.com>
2019-02-18 16:50:43 +0900

    20190218

---
+++ gitinit.bat
@@ -0,0 +1,6 @@
+echo "# Ninja-javascripttips" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/toshiyanishimura1963/Ninja-javascripttips.git
+git push -u origin master
